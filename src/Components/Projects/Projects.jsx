bfff9040880169d328ba69bdc0c3e8a1bea4ee95
Anshita-1609 <anshidilawari@gmail.com>
2025-01-21 19:44:47 +0530

    save changes

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -3,7 +3,7 @@ const projects = [
     {
         name: "Food-App",
         description: "A user-friendly food delivery app with a focus on UI/UX and fast performance.",
-        techStack: ["ReactNative", "TailwindCSS"],
+        techStack: ["React", "TailwindCSS"],
         link: "https://github.com/Anshita-1609/FoodApp",
         image: "/projects/foodapp.png"
     },
